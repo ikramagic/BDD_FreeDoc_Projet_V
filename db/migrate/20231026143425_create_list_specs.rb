f07931f438f76148a56759bfd0205176461833a5
ikramiste <ikrame.saadi@gmail.com>
2023-10-26 19:16:28 +0200

    addex index for recognition

--- db/migrate/20231026143425_create_list_specs.rb
+++ db/migrate/20231026143425_create_list_specs.rb
@@ -1,8 +1,8 @@
 class CreateListSpecs < ActiveRecord::Migration[7.0]
   def change
     create_table :list_specs do |t|
-      t.string :title
-      t.integer :doctor_id
+      t.belongs_to :doctor, index: true
+      t.belongs_to :specialty, index: true
 
       t.timestamps
     end
